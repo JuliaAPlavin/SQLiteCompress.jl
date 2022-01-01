da2d94930a3a2163808cafff0256659f1e840ccb
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    implement

--- src/SQLiteCompress.jl
+++ src/SQLiteCompress.jl
@@ -1,3 +1,59 @@
 module SQLiteCompress
 
+export register_compression!
+
+using TranscodingStreams
+import SQLite
+using DBInterface: execute
+
+
+""" `register_compression!(db::DB, (comp, decomp); do_codec_checks=true)`
+
+Register `compress(TEXT)::BLOB` and `decompress(BLOB)::TEXT` SQL functions in the `db` SQLite database.
+They apply the Julia function `TranscodingStreams.transcode` with provided compressor `comp` and decompressor `decomp`.
+
+- `db`: SQLite database from `SQLite.jl` or compatible
+- `(comp, decomp)`: `TranscodingStreams` codecs, e.g. `(ZstdCompressor, ZstdDecompressor)`
+- `do_codec_checks`: whether to perform sanity checks on provided codecs
+"""
+function register_compression!(db, (comp, decomp); do_codec_checks=true)
+    modul = first(methods(typeof(db))).module  # e.g., SQLite
+    do_codec_checks && _check_comp_decomp(comp, decomp)
+
+    modul.register(db, function compress(x::String)
+        transcode(comp, x)
+    end)
+    modul.register(db, function decompress(x)::String
+        String(transcode(decomp, x))
+    end)
+end
+
+
+function _check_comp_decomp(comp, decomp)
+    str_orig = join(repeat(rand('A':'Z', 100), inner=20))
+    arr_comp = try
+        transcode(comp, str_orig)
+    catch e
+        throw(ArgumentError("Cannot compress a string with provided compressor `$comp`. Got $e."))
+    end
+    str_decomp = try
+        String(transcode(decomp, arr_comp))
+    catch e
+        throw(ArgumentError("Cannot decompress a compressed array with provided decompressor `$decomp`. Got $e."))
+    end
+    str_orig == str_decomp || throw(ArgumentError("Decompressed string is not equal to the original."))
+    length(arr_comp) < length(str_orig) || throw(ArgumentError("Data compressed with `$comp` is not smaller than the original: $(length(arr_comp)) vs $(length(str_orig))."))
+
+    db = SQLite.DB(":memory:")
+    SQLite.@register db function compress(x::String)
+        transcode(comp, x)
+    end
+    SQLite.@register db function decompress(x)::String
+        String(transcode(decomp, x))
+    end
+    if first(execute(db, """select decompress(compress("hello")) as x""")).x != "hello"
+        throw(ArgumentError("Provided [de]compressor doesn't seem to work with SQLite"))
+    end
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,82 @@
 using SQLiteCompress
 using Test
+using Tables
+import SQLite, SQLCipher
+using DBInterface: execute
+using CodecZstd, CodecZlib, CodecBzip2, CodecLz4, CodecXz
 
-@testset begin
+
+@testset "zstd detailed" begin
+    db = SQLite.DB(":memory:")
+
+    @test_throws SQLite.SQLiteException execute(db, """select decompress(compress("hello")) as x""")
+    @test_throws ArgumentError register_compression!(db, (ZstdDecompressor, ZstdCompressor))
+    @test_throws ArgumentError register_compression!(db, (ZstdCompressor, ZstdCompressor))
+    @test_throws ArgumentError register_compression!(db, (ZstdCompressor, ZlibDecompressor))
+    @test_throws SQLite.SQLiteException execute(db, """select decompress(compress("hello")) as x""")
+
+    register_compression!(db, (ZstdCompressor, ZstdDecompressor))
+    @test only(rowtable(execute(db, """select decompress(compress("hello")) as x"""))).x == "hello"
+    @test only(rowtable(execute(db, """select compress("hello") as x"""))).x == transcode(ZstdCompressor, "hello")
+    @test only(rowtable(execute(db, """select decompress(compress(decompress(compress("hello")))) == decompress(compress("hello")) as x"""))).x == 1
+    
+    execute(db, "create table tbl ( x text not null check (typeof(x) = 'text'), y blob not null check (typeof(y) = 'blob') )")
+    execute(db, "insert into tbl (x, y) values (?, ?)", ("hello", transcode(ZstdCompressor, "hello")))
+    longstrs = [join(rand('A':'Z', 1000)) for _ in 1:2]
+    execute(db, "insert into tbl (x, y) values (?, ?)", (longstrs[1], transcode(ZstdCompressor, longstrs[1])))
+    execute(db, "insert into tbl (x, y) values (?, ?)", (longstrs[2], transcode(ZstdCompressor, longstrs[2])))
+    
+    res = rowtable(execute(db, "select x, y, decompress(y) as dy, compress(x) as cx, typeof(decompress(y)) as tdy from tbl"))
+    @test all([r.tdy == "text" for r in res])
+    @test all([r.x == r.dy for r in res])
+    @test all([r.cx == r.y for r in res])
+    @test all(r.ok == 1 for r in rowtable(execute(db, "select y == compress(x) as ok from tbl")))
+    @test all(r.ok == 1 for r in rowtable(execute(db, "select quote(decompress(y)) == quote(x) as ok from tbl")))
+
+    # SQLite.jl seems to return a nul-terminated string from UDF
+    # it doesn't compare equal in SQL, can be checked with hex()
+    @test_broken only(rowtable(execute(db, """select decompress(compress("hello")) == "hello" as x"""))).x == 1
+    @test_broken all(r.ok == 1 for r in rowtable(execute(db, "select decompress(y) == x as ok from tbl")))
+end
+
+
+@testset "all simple" begin
+    @testset for cdecomp in [
+            (ZstdCompressor, ZstdDecompressor),
+            (ZlibCompressor, ZlibDecompressor),
+            (GzipCompressor, GzipDecompressor),
+            (DeflateCompressor, DeflateDecompressor),
+            (Bzip2Compressor, Bzip2Decompressor),
+            (LZ4FastCompressor, LZ4SafeDecompressor),
+            (LZ4HCCompressor, LZ4SafeDecompressor),
+            (XzCompressor, XzDecompressor),
+        ]
+        db = SQLite.DB(":memory:")
+        @test_throws SQLite.SQLiteException execute(db, """select decompress(compress("hello")) as x""")
+        @test_throws ArgumentError register_compression!(db, reverse(cdecomp))
+        register_compression!(db, cdecomp)
+        @test only(rowtable(execute(db, """select decompress(compress("hello")) as x"""))).x == "hello"
+    end
+end
+
+
+@testset "sqlcipher" begin
+    @testset for cdecomp in [
+            (ZstdCompressor, ZstdDecompressor),
+            (ZlibCompressor, ZlibDecompressor),
+            (GzipCompressor, GzipDecompressor),
+            (DeflateCompressor, DeflateDecompressor),
+            (Bzip2Compressor, Bzip2Decompressor),
+            (LZ4FastCompressor, LZ4SafeDecompressor),
+            (LZ4HCCompressor, LZ4SafeDecompressor),
+            (XzCompressor, XzDecompressor),
+        ]
+        db = SQLCipher.DB(":memory:")
+        @test_throws SQLCipher.SQLiteException execute(db, """select decompress(compress("hello")) as x""")
+        @test_throws ArgumentError register_compression!(db, reverse(cdecomp))
+        register_compression!(db, cdecomp)
+        @test only(rowtable(execute(db, """select decompress(compress("hello")) as x"""))).x == "hello"
+    end
 end
 
 
